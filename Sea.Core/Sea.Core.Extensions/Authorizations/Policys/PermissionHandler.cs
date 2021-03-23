501064cd67d11d78f9a2318b3a505984a951b8cb
SeaLee <654029739@qq.com>
2021-03-23 18:01:32 +0800

    初始化项目

---
+++ Sea.Core/Sea.Core.Extensions/Authorizations/Policys/PermissionHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sea.Core.Extensions.Authorizations.Policys
+{
+    /// <summary>
+    /// 权限授权处理器
+    /// </summary>
+    public class PermissionHandler : AuthorizationHandler<PermissionRequirement>
+    {
+        /// <summary>
+        /// 验证方案提供对象
+        /// </summary>
+        public IAuthenticationSchemeProvider Schemes { get; set; }
+        //private readonly IRoleModulePermissionServices _roleModulePermissionServices; IRoleModulePermissionServices roleModulePermissionServices, 
+        private readonly IHttpContextAccessor _accessor;
+
+        /// <summary>
+        /// 构造函数注入
+        /// </summary>
+        /// <param name="schemes"></param>
+        /// <param name="roleModulePermissionServices"></param>
+        /// <param name="accessor"></param>
+        public PermissionHandler(IAuthenticationSchemeProvider schemes, IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+            Schemes = schemes;
+            //_roleModulePermissionServices = roleModulePermissionServices;
+        }
+
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
