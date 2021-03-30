acc311e0e455cc2e48b3065e5a188f0e400159fb
SeaLee <654029739@qq.com>
2021-03-30 18:11:55 +0800

    添加API基类

---
+++ Sea.Core/Src/Sea.Core.Api/Controllers/00Abstract/ControllerAbstract.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sea.Core.Api.Controllers.Abstract
+{
+    /// <summary>
+    /// 控制器基础类
+    /// </summary>
+    [ApiController]
+    [Route("api/[area]/[controller]/[action]")]
+    //[Authorize(Permissions.Name)]
+    public abstract class ControllerAbstract : ControllerBase
+    { 
+        
+
+
+    }
+}
